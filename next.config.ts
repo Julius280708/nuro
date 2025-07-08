a6ba41fef5d2c6b558c88fdff96159ef70a54521
Julius Janser <julius@MacBookPro.fritz.box>
2025-07-08 22:08:03 +0200

    Added upload of pictures

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
